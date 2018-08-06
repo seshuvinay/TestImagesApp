c2692e6ba7c10cc981ebc075788218cdfe0fca4a
vinay <seshu.vinay@gmail.com>
2018-08-06 13:33:27 +0530

    error fix

--- app/src/main/java/com/example/vinay/testimagesapp/MainActivity.java
+++ app/src/main/java/com/example/vinay/testimagesapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.vinay.testimagesapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -15,21 +17,25 @@ public class MainActivity extends AppCompatActivity {
     private long start1;
     private long start2;
     private long start3;
-    private long start4;
-    private long start5;
+    ScrollView scroll1, scroll2, scroll3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scroll1 = findViewById(R.id.scroll1);
+        scroll2 = findViewById(R.id.scroll2);
+
+        scroll3 = findViewById(R.id.scroll3);
+
         findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 download1();
                 download2();
                 download3();
-                download4();
-                download5();
+
                 findViewById(R.id.submit).setVisibility(View.GONE);
             }
         });
@@ -42,16 +48,31 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                     long duration = System.currentTimeMillis() - start1;
-                    ((TextView) findViewById(R.id.info1)).setText("Image 1: " + (duration));
+                    String text = ((TextView) findViewById(R.id.info1)).getText().toString();
+                    ((TextView) findViewById(R.id.info1)).setText(text + "\nImage 1: " + (duration));
+                    scroll1.fullScroll(View.FOCUS_DOWN);
                     download1();
                 }
 
                 @Override
                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    ((TextView) findViewById(R.id.info1)).setText("Image 1 failed");
+                    String text = ((TextView) findViewById(R.id.info1)).getText().toString();
+                    ((TextView) findViewById(R.id.info1)).setText(text + "\nImage 1 failed" + statusCode);
+                    scroll1.fullScroll(View.FOCUS_DOWN);
                     download1();
                 }
             });
+        } else {
+            String text = ((TextView) findViewById(R.id.info1)).getText().toString();
+
+            ((TextView) findViewById(R.id.info1)).setText(text + "\nImage 1 empty");
+            scroll1.fullScroll(View.FOCUS_DOWN);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    download1();
+                }
+            },1000);
         }
 
 
@@ -64,16 +85,33 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                     long duration = System.currentTimeMillis() - start2;
-                    ((TextView) findViewById(R.id.info2)).setText("Image 2: " + (duration));
+                    String text = ((TextView) findViewById(R.id.info2)).getText().toString();
+
+                    ((TextView) findViewById(R.id.info2)).setText(text + "\nImage 2: " + (duration));
+                    scroll2.fullScroll(View.FOCUS_DOWN);
                     download2();
                 }
 
                 @Override
                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    ((TextView) findViewById(R.id.info2)).setText("Image 2 failed");
+                    String text = ((TextView) findViewById(R.id.info2)).getText().toString();
+
+                    ((TextView) findViewById(R.id.info2)).setText(text + "\nImage 2 failed" + statusCode);
+                    scroll2.fullScroll(View.FOCUS_DOWN);
                     download2();
                 }
             });
+        } else {
+            String text = ((TextView) findViewById(R.id.info2)).getText().toString();
+
+            ((TextView) findViewById(R.id.info2)).setText(text + "\nImage 2 empty");
+            scroll2.fullScroll(View.FOCUS_DOWN);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    download2();
+                }
+            },1000);
         }
 
     }
@@ -85,62 +123,37 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                     long duration = System.currentTimeMillis() - start3;
-                    ((TextView) findViewById(R.id.info3)).setText("Image 3: " + (duration));
+                    String text = ((TextView) findViewById(R.id.info3)).getText().toString();
+
+                    ((TextView) findViewById(R.id.info3)).setText(text + "\nImage 3: " + (duration));
+                    scroll3.fullScroll(View.FOCUS_DOWN);
                     download3();
                 }
 
                 @Override
                 public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    ((TextView) findViewById(R.id.info3)).setText("Image 3 failed");
+                    String text = ((TextView) findViewById(R.id.info3)).getText().toString();
+
+                    ((TextView) findViewById(R.id.info3)).setText(text + "\nImage 3 failed" + statusCode);
+                    scroll3.fullScroll(View.FOCUS_DOWN);
                     download3();
                 }
             });
+        } else {
+            String text = ((TextView) findViewById(R.id.info3)).getText().toString();
+
+            ((TextView) findViewById(R.id.info3)).setText(text + "\nImage 3 empty");
+            scroll3.fullScroll(View.FOCUS_DOWN);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    download3();
+                }
+            },1000);
         }
 
 
     }
 
-    private void download4() {
-        if (!((TextView) findViewById(R.id.img4)).getText().toString().trim().equals("")) {
-            start4 = System.currentTimeMillis();
-            new AsyncHttpClient().get(((TextView) findViewById(R.id.img4)).getText().toString().trim(), new AsyncHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                    long duration = System.currentTimeMillis() - start4;
-                    ((TextView) findViewById(R.id.info4)).setText("Image 4: " + (duration));
-                    download4();
-                }
 
-                @Override
-                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    ((TextView) findViewById(R.id.info4)).setText("Image 4 failed");
-                    download4();
-                }
-            });
-        }
-
-
-    }
-
-    private void download5() {
-        if (!((TextView) findViewById(R.id.img5)).getText().toString().trim().equals("")) {
-            start5 = System.currentTimeMillis();
-            new AsyncHttpClient().get(((TextView) findViewById(R.id.img5)).getText().toString().trim(), new AsyncHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                    long duration = System.currentTimeMillis() - start5;
-                    ((TextView) findViewById(R.id.info5)).setText("Image 5: " + (duration));
-                    download5();
-                }
-
-                @Override
-                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                    ((TextView) findViewById(R.id.info5)).setText("Image 5 failed");
-                    download5();
-                }
-            });
-        }
-
-
-    }
 }
